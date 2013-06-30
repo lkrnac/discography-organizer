10abc8a72dd60819b2cf4b98b8569b5823e0aac0
sitko <sitkoo@gmail.com>
2013-06-30 09:53:16 +0100

    JaCoCo test coverage report generation

---
+++ discography-organizer.tests/src/main/java/sk/lkrnac/discorg/JacocoDummyClass.java
@@ -0,0 +1,13 @@
+package sk.lkrnac.discorg;
+
+/**
+ * This class is needed because JaCoCo is failing to generate a coverage report
+ * when project doesn't have classes in src/main/java. This issue is discussed
+ * <a href="https://github.com/jacoco/jacoco/issues/67">JaCoCo GitHub site</a>.
+ * 
+ * @author sitko
+ * 
+ */
+public class JacocoDummyClass {
+
+}
